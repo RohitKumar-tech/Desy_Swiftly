e0a5434d80e0086c6c08a1d745d8b9b795e67d92
rohitkumar <rohitkumar620200@gmail.com>
2024-12-23 12:38:59 +0530

    Second commit

--- SERVER/src/config/connect.js
+++ SERVER/src/config/connect.js
@@ -3,9 +3,8 @@ import mongoose  from "mongoose";
 export const connectDB = async(uri) => {
     try{
         await mongoose.connect(uri)
-        console.log("DB CONNECTED ✅ ")
-    }
-    catch (error) {
+        console.log("DB CONNECTED ✅")
+    } catch (error) {
         console.log("Database connection error : ",error)
     }
 }
